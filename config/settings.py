2dae0cb4a364714191596dd849da8c4d62ce3d72
izumichiDana <kurbanbekovadana@gmail.com>
2022-05-18 17:41:22 +0800

    psql, swagger

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'drf_yasg',
+    'config',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +80,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'api_db',
+        'USER':config('DB_USER'),
+        'PASSWORD': config('DB_PASS'),
+        'HOST':'127.0.0.1',
+        'PORT':5432
+
     }
 }
 
--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Fullstack_hakaton',
+        default_version='v1',
+        description='My API'
+    ),
+    public=True
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
