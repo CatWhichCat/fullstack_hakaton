75e712676dfe28faf55ba40cc4316122f48bf3f3
izumichiDana <kurbanbekovadana@gmail.com>
2022-05-20 17:48:00 +0800

    rabotaet

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.routers import DefaultRouter
-from product.views import toggle_like
+from product.views import CategoryViewSet, toggle_like
 from product.views import ProductViewSet
 
 # Swagger
@@ -17,6 +17,7 @@ schema_view = get_schema_view(
 )
 router = DefaultRouter()
 router.register('products', ProductViewSet)
+router.register('category', CategoryViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
