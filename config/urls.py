047e0530275d94e4e2c38ff00543df4d4a091e8e
izumichiDana <kurbanbekovadana@gmail.com>
2022-05-22 12:15:20 +0800

    fix bugs

--- config/urls.py
+++ config/urls.py
@@ -24,10 +24,10 @@ router.register('products', ProductViewSet)
 router.register('category', CategoryViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', schema_view.with_ui('swagger', cache_timeout=0)),
-    path('api/v1/', include('myaccount.urls')),
+    path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
-    path('api/v1/products/<int:id>/toggle_like/', toggle_like),
+    path('api/v1/myaccount/', include('myaccount.urls')),
+    path('api/v1/products/<int:id>/toggle_like/', toggle_like)
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
